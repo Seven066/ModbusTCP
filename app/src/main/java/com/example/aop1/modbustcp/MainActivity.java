feb8462859384f935ffee9cd64d30ba19b3d07ed
Seven066 <aopl066@gmail.com>
2016-08-24 12:52:26 +0700

    init

---
+++ app/src/main/java/com/example/aop1/modbustcp/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.aop1.modbustcp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    final String LOGTAG = "MyLog";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //регистрация кнопок запуска активити
+        Button bStartSlaveActivity = (Button) findViewById(R.id.bStartSlaveActivity);
+        bStartSlaveActivity.setOnClickListener(this);
+        Button bStartMasterActivity = (Button) findViewById(R.id.bStartMasterActivity);
+        bStartMasterActivity.setOnClickListener(this);
+        //--
+
+    }
+
+    //обработка нажатий кнопок
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.bStartMasterActivity:
+                Log.d(LOGTAG,"Master button pressed");
+                Intent intent = new Intent(this, MasterActivity.class);
+                break;
+            case R.id.bStartSlaveActivity:
+                Log.d(LOGTAG,"Slave button pressed");
+                break;
+            default:
+                Log.d(LOGTAG,"Нажато не пойми что");
+                break;
+        }
+    }
+}
---
+++ app/src/main/java/com/example/aop1/modbustcp/SlaveActivity.java
@@ -0,0 +1,13 @@
+package com.example.aop1.modbustcp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SlaveActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_slave);
+    }
+}
